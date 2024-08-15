0e5d2980bb4255f83a48c347dd1dfb988fc0050e
이건희 <105201451+dnzp75@users.noreply.github.com>
2024-08-16 04:58:41 +0900

    Revert "branch push"

--- src/main/java/com/example/ci/cd_test_server/AppController.java
+++ src/main/java/com/example/ci/cd_test_server/AppController.java
@@ -1,9 +1,6 @@
 package com.example.ci.cd_test_server;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,11 +9,6 @@ public class AppController {
     @GetMapping("/")
     public  String home(){
         return "Hi, World! LeeGeonHui";
-    }
-
-    @GetMapping("/")
-    public  String home2(){
-        return "Hi, World! LeeGeonHuifdsfasdfasdfasdfasdfasdfsadf";
     }
 
 }
