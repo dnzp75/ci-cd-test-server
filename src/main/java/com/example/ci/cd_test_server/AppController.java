6d9cb909be39a9562662b92b2c6bc755fc4e4ef7
이건희 <dnzp74@g.eulji.ac.kr>
2024-08-16 05:05:49 +0900

    branch3 push

--- src/main/java/com/example/ci/cd_test_server/AppController.java
+++ src/main/java/com/example/ci/cd_test_server/AppController.java
@@ -8,7 +8,7 @@ public class AppController {
 
     @GetMapping("/")
     public  String home(){
-        return "Hi, World! LeeGeonHui";
+        return "Hi, World! LeeGeonHuiasfddfasdfasdfasdfasdfasdfasdf";
     }
 
     @GetMapping("/ass")
