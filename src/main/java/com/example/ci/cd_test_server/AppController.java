bdcd1ec182f4d83589a6491a1c6e36f3d35f2c88
이건희 <dnzp74@g.eulji.ac.kr>
2024-08-20 08:43:52 +0900

    cd-test

--- src/main/java/com/example/ci/cd_test_server/AppController.java
+++ src/main/java/com/example/ci/cd_test_server/AppController.java
@@ -8,7 +8,7 @@ public class AppController {
 
     @GetMapping("/")
     public  String home(){
-        return "Hi, World! LeeGeonHuiasfddfasdfasdfasdfasdfasdfasdf";
+        return "Hi, World! ";
     }
 
     @GetMapping("/ass")
