3927c950d651943b98bce6c4f36e862fb4fccc61
이건희 <dnzp74@g.eulji.ac.kr>
2024-08-20 07:27:31 +0900

    실패하는 테스트 추가

--- src/test/java/com/example/ci/cd_test_server/ApplicationTests.java
+++ src/test/java/com/example/ci/cd_test_server/ApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.ci.cd_test_server;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+
 class ApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void testHome2Endpoint() throws Exception {
+		mockMvc.perform(get("/ass"))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.content().string("This will fail"));  // 잘못된 예상 결과
+	}
+
+
 
 }
